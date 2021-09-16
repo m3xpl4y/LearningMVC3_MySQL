c8d881cec872e08c4f9cecd6ce3a91965fac0d77
m3xpl4y <m3xpl4y@gmail.com>
2021-09-16 19:51:41 +0200

    added AdministrationController, Create Role ViewModels

---
+++ LearningMVC3_MySQL/Controllers/AdministrationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningMVC3_MySQL.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> rolemanager;
+
+        public AdministrationController(RoleManager<IdentityRole> rolemanager)
+        {
+            this.rolemanager = rolemanager;
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+    }
+}
---
+++ LearningMVC3_MySQL/ViewModel/CreateRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningMVC3_MySQL.ViewModel
+{
+    public class CreateRoleViewModel
+    {
+        public string Id { get; set; }
+        [Required]
+        public string RoleName { get; set; }
+    }
+}
