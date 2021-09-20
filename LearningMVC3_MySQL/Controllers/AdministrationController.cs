7ff488e33860ae22914cda98cd6392da87d226f7
m3xpl4y <m3xpl4y@gmail.com>
2021-09-20 13:53:16 +0200

    finished roles

--- LearningMVC3_MySQL/Controllers/AdministrationController.cs
+++ LearningMVC3_MySQL/Controllers/AdministrationController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace LearningMVC3_MySQL.Controllers
 {
-    
+    [Authorize(Roles = "Admin")]
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> rolemanager;
@@ -54,7 +54,6 @@ namespace LearningMVC3_MySQL.Controllers
         }
 
         //List of all Available Roles
-        [Authorize]
         [HttpGet]
         public IActionResult ListRoles()
         {
@@ -132,8 +131,8 @@ namespace LearningMVC3_MySQL.Controllers
                 return View("Not Found");
             }
             var model = new List<UserRoleViewModel>();
-            var users = await UserManager.GetUsersInRoleAsync(role.Id);
-            foreach (var user in users)
+
+            foreach (var user in UserManager.Users.ToList())
             {
                 var userRoleViewModel = new UserRoleViewModel
                 {
@@ -152,6 +151,48 @@ namespace LearningMVC3_MySQL.Controllers
             }
             
             return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> model, string roleId)
+        {
+            var role = await rolemanager.FindByIdAsync(roleId);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with ID = {roleId} cannot be found";
+                return View("Not Found");
+            }
+            for (int i = 0; i < model.Count; i++)
+            {
+                var user = await UserManager.FindByIdAsync(model[i].UserId);
+                IdentityResult identityResult = null;
+
+                if(model[i].isSelected && !(await UserManager.IsInRoleAsync(user, role.Name)))
+                {
+                    identityResult = await UserManager.AddToRoleAsync(user, role.Name);
+                }
+                else if (!model[i].isSelected && await UserManager.IsInRoleAsync(user, role.Name))
+                {
+                    identityResult = await UserManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                    //Because there are 2 more options what can be, if the user is selected and already in the role
+                    //we do not what to do anything
+                    //if the user is not selected and not in the role
+                    //we do not what to do anything 
+                    //so continue in code
+                }
+                if(identityResult.Succeeded)
+                {
+                    if (i < (model.Count) - 1)
+                        continue;
+                    else
+                        return RedirectToAction("EditRole", new { Id = roleId });
+                }
+            }
+
+            return RedirectToAction("EditRole", new { Id = roleId });
         }
     }
 }
