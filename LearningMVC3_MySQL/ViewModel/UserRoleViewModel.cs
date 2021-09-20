8b161dd32a2267839c1495fa480b25a2eba75cfe
m3xpl4y <m3xpl4y@gmail.com>
2021-09-20 11:56:38 +0200

    UserViewModel added/created

---
+++ LearningMVC3_MySQL/ViewModel/UserRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningMVC3_MySQL.ViewModel
+{
+    public class UserRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public bool isSelected { get; set; }
+    }
+}
