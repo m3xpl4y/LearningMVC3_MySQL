1c9abf6ddaf478dc947c93530261d5698016dc81
m3xpl4y <m3xpl4y@gmail.com>
2021-09-21 16:51:14 +0200

    added upon register, user assign role

--- LearningMVC3_MySQL/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LearningMVC3_MySQL/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -88,8 +88,11 @@ namespace LearningMVC3_MySQL.Areas.Identity.Pages.Account
             {
                 var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName, Gender = Input.Gender, City = Input.City };
                 var result = await _userManager.CreateAsync(user, Input.Password);
+                
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, "User");
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
