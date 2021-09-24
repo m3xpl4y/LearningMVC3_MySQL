ea35668ade909a5c32b86ecf2329f18939aba52b
m3xpl4y <m3xpl4y@gmail.com>
2021-09-24 11:52:07 +0200

    changed layout that if user is logged in is darkmode if not logged in light mode

--- LearningMVC3_MySQL/wwwroot/js/site.js
+++ LearningMVC3_MySQL/wwwroot/js/site.js
@@ -12,6 +12,7 @@ function toggleButton() {
     if (x.style.display === "block") {
         x.style.display = "none";
         y.style.display = "block";
+        
     } else {
         x.style.display = "block";
         y.style.display = "none";
